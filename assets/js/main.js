b16681d0df49287bfe3f076f669de146f1a6c672
aadharkulshrestha <deepakulshrestha13@gmail.com>
2021-06-01 16:50:52 +0530

    Firebase Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,64 +1,65 @@
-/*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
-
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
-    }
-}
-showMenu('nav-toggle','nav-menu')
-
-/*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav__link');   
-
-function linkAction(){
-  /*Active link*/
-  navLink.forEach(n => n.classList.remove('active'));
-  this.classList.add('active');
+// Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyAPPU4kCZf9oRYRuYy_hFYx6DBwcRLSr4I",
+    authDomain: "portfolio-d028c.firebaseapp.com",
+    projectId: "portfolio-d028c",
+    storageBucket: "portfolio-d028c.appspot.com",
+    messagingSenderId: "943977931501",
+    appId: "1:943977931501:web:d7cc81c95125772643a2e4",
+    measurementId: "G-4SKWW7XRYS"
+  };
   
-  /*Remove menu mobile*/
-  const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show')
+firebase.initializeApp(firebaseConfig);
+
+// Reference messages collection
+var queryRef = firebase.database().ref('Queries');
+
+
+
+/* SUBMIT BUTTON */
+document.getElementById('contactForm').addEventListener('submit' , submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+
+  //Get VAlues
+
+  var name = getInputVal("name");
+  var email = getInputVal("email");
+  var question = getInputVal("question");
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+
+
+    
+    //Save Message
+    saveMessage(name, email, question);
+    
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
-
-/*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-});
-
-/*SCROLL HOME*/
-sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-
-/*SCROLL ABOUT*/
-sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
-
-/*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
-
-/*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
-
-/*SCROLL Project*/
-sr.reveal('.project__img',{interval: 200}); 
-
-/*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 200}); 
 
 
+// Get Input Values
+function getInputVal(id) {
+    return document.getElementById(id).value;
+}
 
+// Save message to firebase
+function saveMessage(name, email, question){
+    var newQueryRef = queryRef.push();
+    newQueryRef.set({
+      name: name,
+      email:email,
+      question: question
+    });
+  }
+  
 
