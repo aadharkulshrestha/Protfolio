a61930012011191b7dbd1b4e46b8ccf987c0cd12
aadharkulshrestha <deepakulshrestha13@gmail.com>
2021-06-02 13:47:11 +0530

    updated

--- assets/js/scrool.js
+++ assets/js/scrool.js
@@ -45,17 +45,17 @@ sr.reveal('.about__subtitle',{delay: 400});
 sr.reveal('.about__text',{delay: 400}); 
 
 /*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+// sr.reveal('.skills__subtitle',{}); 
+// sr.reveal('.skills__text',{}); 
+// sr.reveal('.skills__data',{interval: 200}); 
+// sr.reveal('.skills__img',{delay: 600});
 
 /*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 100}); 
+// sr.reveal('.work__img',{interval: 100}); 
 
 /*SCROLL Project*/
-sr.reveal('.project__img',{interval: 100}); 
+//sr.reveal('.project__img',{interval: 100}); 
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 100}); 
+//sr.reveal('.contact__input',{interval: 100}); 
 
