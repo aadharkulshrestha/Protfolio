33fbd41fa8d227053b0d7be2ef79396304a2d919
Aadhar Kulshrestha <aadhar Kulshrestha3150@gmail.com>
2021-06-02 14:58:42 +0530

    FInal Update

--- assets/js/scrool.js
+++ assets/js/scrool.js
@@ -35,7 +35,7 @@ const sr = ScrollReveal({
 
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
+sr.reveal('.button',{delay: 0}); 
 sr.reveal('.home__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 
