dc192a29b84e946c54c35497c84ff137d71156c4
aadharkulshrestha <deepakulshrestha13@gmail.com>
2021-06-23 16:47:18 +0530

    Updated

--- assets/js/scrool.js
+++ assets/js/scrool.js
@@ -33,16 +33,16 @@ const sr = ScrollReveal({
     reset: true
 });
 
-/*SCROLL HOME*/
-sr.reveal('.home__title',{}); 
-//sr.reveal('.button',{delay: 0}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+// /*SCROLL HOME*/
+// sr.reveal('.home__title',{}); 
+// //sr.reveal('.button',{delay: 0}); 
+// sr.reveal('.home__img',{delay: 100}); 
+// sr.reveal('.home__social-icon',{ interval: 100}); 
 
-/*SCROLL ABOUT*/
-sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+// /*SCROLL ABOUT*/
+// sr.reveal('.about__img',{}); 
+// sr.reveal('.about__subtitle',{delay: 100}); 
+// sr.reveal('.about__text',{delay: 100}); 
 
 /*SCROLL SKILLS*/
 // sr.reveal('.skills__subtitle',{}); 
