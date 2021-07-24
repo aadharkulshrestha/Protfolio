f9e9876a8e4fde5a366768ee7cb0a501244eeec9
aadharkulshrestha <deepakulshrestha13@gmail.com>
2021-07-24 11:21:38 +0530

    UPDATED

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,24 @@
 // Initialize Firebase
 var firebaseConfig = {
-    apiKey: "AIzaSyAPPU4kCZf9oRYRuYy_hFYx6DBwcRLSr4I",
-    authDomain: "portfolio-d028c.firebaseapp.com",
-    projectId: "portfolio-d028c",
-    storageBucket: "portfolio-d028c.appspot.com",
-    messagingSenderId: "943977931501",
-    appId: "1:943977931501:web:d7cc81c95125772643a2e4",
-    measurementId: "G-4SKWW7XRYS"
-  };
-  
+  apiKey: "AIzaSyAPPU4kCZf9oRYRuYy_hFYx6DBwcRLSr4I",
+  authDomain: "portfolio-d028c.firebaseapp.com",
+  projectId: "portfolio-d028c",
+  storageBucket: "portfolio-d028c.appspot.com",
+  messagingSenderId: "943977931501",
+  appId: "1:943977931501:web:d7cc81c95125772643a2e4",
+  measurementId: "G-4SKWW7XRYS",
+};
+
 firebase.initializeApp(firebaseConfig);
 
 // Reference messages collection
-var queryRef = firebase.database().ref('Queries');
-
-
+var queryRef = firebase.database().ref("Queries");
 
 /* SUBMIT BUTTON */
-document.getElementById('contactForm').addEventListener('submit' , submitForm);
+document.getElementById("contactForm").addEventListener("submit", submitForm);
 
 function submitForm(e) {
-    e.preventDefault();
+  e.preventDefault();
 
   //Get VAlues
 
@@ -29,37 +27,31 @@ function submitForm(e) {
   var question = getInputVal("question");
 
   // Show alert
-  document.querySelector('.alert').style.display = 'block';
+  document.querySelector(".alert").style.display = "block";
 
   // Hide alert after 3 seconds
-  setTimeout(function(){
-    document.querySelector('.alert').style.display = 'none';
-  },3000);
+  setTimeout(function () {
+    document.querySelector(".alert").style.display = "none";
+  }, 2000);
 
   // Clear form
-  document.getElementById('contactForm').reset();
+  document.getElementById("contactForm").reset();
 
-
-    
-    //Save Message
-    saveMessage(name, email, question);
-    
+  //Save Message
+  saveMessage(name, email, question);
 }
-
 
 // Get Input Values
 function getInputVal(id) {
-    return document.getElementById(id).value;
+  return document.getElementById(id).value;
 }
 
 // Save message to firebase
-function saveMessage(name, email, question){
-    var newQueryRef = queryRef.push();
-    newQueryRef.set({
-      name: name,
-      email:email,
-      question: question
-    });
-  }
-  
-
+function saveMessage(name, email, question) {
+  var newQueryRef = queryRef.push();
+  newQueryRef.set({
+    name: name,
+    email: email,
+    question: question,
+  });
+}
